7f4b16cc659957d9acb912876ce57afc01400902
Rajko Vrga <rajkovrga@yahoo.com>
2018-10-28 23:59:31 +0100

    add ping

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace PingRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Ping pn = new Ping())
+            {
+                Request r = new Request("www.facebook.com", pn);
+
+
+                
+
+                for (int i = 0; i < 5; i++)
+                {
+                    r.reguestPing();
+                    Thread.Sleep(5000);
+                }
+            }
+            Console.WriteLine("Kraj");
+            Console.ReadLine();
+            
+        }
+    }
+
+}
---
+++ Request.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace PingRequest
+{
+    class Request 
+    {
+
+        private string adr;
+        private Ping ping;
+
+        public Request(string adr,Ping ping)
+        {
+            this.adr = adr;
+            this.ping = ping;
+        }
+
+      
+        public void reguestPing()
+        {
+    
+            PingReply pr = this.ping.Send(this.adr);
+            if (pr.Status == IPStatus.Success)
+            {
+                Console.WriteLine(pr.Address + " " + pr.Status + " " + pr.Options.Ttl);
+            }
+            else
+            {
+                Console.WriteLine(pr.Status + " ");
+            }
+
+        }
+
+    }
+}
